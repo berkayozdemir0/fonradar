1262a5fe0b7a309545b3a9f3839b03171a954873
Berkay Özdemir <ozdemirberkaytr@gmail.com>
2022-12-15 22:04:34 +0000

    input-validation-ok

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const togglePassword = 
         document.querySelector('#togglePassword');
           
-    const password = document.querySelector('#id_password');
+    const password = document.querySelector('#fnumber');
   
     togglePassword.addEventListener('click', function (e) {
   
@@ -20,3 +20,59 @@ const togglePassword =
 "images/eye-show.png";
         }
     });
+
+
+
+
+
+// input validation start
+
+    function check () {
+        // (C1) INIT
+        var valid = true, error = "", field = "";
+      
+        // (C2) NAME
+        field = document.getElementById("fname");
+        error = document.getElementById("cname");
+        if (!field.checkValidity()) {
+          valid = false;
+          field.classList.add("err");
+          error.innerHTML = "Lütfen e posta adresinizi giriniz\r\n";
+        } else {
+          field.classList.remove("err");
+          error.innerHTML = "";
+        }
+      
+        // (C3) NUMBER
+        field = document.getElementById("fnumber");
+        error = document.getElementById("fpassword");
+        if (!field.checkValidity()) {
+          valid = false;
+          field.classList.add("err");
+          error.innerHTML = "Lütfen şifrenizi giriniz\r\n";
+        } else {
+          field.classList.remove("err");
+          error.innerHTML = "";
+        }
+      
+        // (C4) RESULT
+        return valid;
+      }
+    //   input validation finish
+
+    // input rediretc start 
+    window.addEventListener("load", function() { // when the page has loaded
+        document.getElementById("myForm").addEventListener("submit", function(e) { // passing the event
+          e.preventDefault(); // you do not want to let the form submit because you handle the nex page 
+          const username = document.getElementById('fname').value;
+          const password = document.getElementById('fpassword').value;
+      
+          if (username === "berkay" && password === 'berkay') {
+            // alert('You have sucessfully logged in');
+            window.location.href = "http://fonradar.com";
+          } else {
+            alert('Wrong username or password');
+          }
+        })
+      })
+    //   input redirect finish 
